a5242d94962a5bc1d8368cbd326946f20d2748c1
AnshumanRShar <59237628+AnshumanRShar@users.noreply.github.com>
2025-10-22 01:16:39 -0700

    additions to description and /build file zipped

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -37,13 +37,33 @@ function Home2() {
             <p className="home-about-body">
               Internet Yami-ichi
 
-S*%t happens. Didn't used to. Like when some small off-handed private comment turns into an SNS flame war. Or worrying about not getting enough “Likes”. The Youtube videos you can't watch anymore because somebody complained. Or the apps you can't play because they were rejected by Apple. Once upon a time, the Internet was supposed to be a place for "liberty". Nowadays it's so uptight. So let's turn off, log-out, and drop in, on the real world. The Internet Yami-Ichi is a flea market for “browsing” face-to-face. Take your own Internet liberties* here, with us. * But no dangerous or illegal goods, please! This black market wants to remain free and useful!
-Internet Yami-ichi* (Internet Black Market) is flea market-style event for buying and selling internet-ish goods and services in real life.
-The internet is not what it used to be. Especially with the rise of social media, and ubiquitous smart-phones permanently on-line, our off-line lives have become increasingly inter-dependent upon our on-line lives. The organization of this much of society on this common infrastructure necessitates the establishment of rules which have made it a lot more uptight.
-The Internet Yami-ichi was born from our inalienable right to turn off, log out, and meet up, and enjoy what we want in an un-monitored new space, and confirm that everything that we enjoyed on-line was put there by one of us, and we can enjoy it together off-line too.
-Like the early Internet, the Yami-Ichi atmosphere is rich with irony and humor. The best of what we share and exchange at the Yami-Ichi is popular for being witty, playful, provocative.
-After the first event started in 2012, it has been spreading to 12 cities in the world, such as Tokyo, Berlin, Brussels, Amsterdam and New York.*Yami-Ichi means black market in Japanese, and the word Yami also implies "sick for" and "addicted to". We call it black market only because "when we see the internet as the front distribution, that makes the "reality" the back distribution", not because we want to break the law. Ours is a bright black market without illegal stuff.
+The class DMA 111 - Art and the Internet presents to you Yami-Ichi
+In a class that explores internet culture, its development, and current presence, each student has focused on a theme that dissects a particular dialogue of the web.
+From identity to privacy, different matters are discussed through a flea market of trading goods. Attendees are encouraged to bring a mobile device, given that objects in the stands aren’t limited to the physical, some also live digitally.
               <br />
+              <br />
+
+Read about some of the Yami-Ichi students’ work:
+              <br />
+              <br />
+
+Glitter gifs, all shades of pink, and dubious ads— go back to the landscape of early 2010’s online girls games! The space targeting young girls online reflected society’s standards for women, thin, white, blonde, heterosexual, and cisgender. In these games, kids were allowed to play with gender, but the options were always strictly feminine. As a trans man, I wanted to make a game that reflected my pretransition experience growing up as a queer girl playing these games, but replace the barbie aesthetic with stereotypical trans masc clothing at the time.
+              <br />
+~Ash
+              <br />
+              <br />
+
+EmoMart™ is a fictional convenience store where emotions are packaged, priced, and sold like everyday products. Each receipt records a different feeling — anger, anxiety, sadness, or happiness — triggered by real posts and phrases circulating online. By turning fleeting digital emotions into tangible transactions, EmoMart™ reflects how the internet commodifies feelings, transforming genuine human experiences into consumable moments for clicks, likes, and validation.
+              <br />
+~Jiayi
+              <br />
+              <br />
+
+Book*’s contents consist of the terms of service dictating its purchase. Users can either read through the entire terms and conditions to decrypt the key and earn the book, or blindly sign at the end of the document— binding them to pay a sum unknown to them until their signature. The project reflects the predatory Terms of Service and EULAs that govern our lived experiences in both hardware and software.
+              <br />
+~Calvin McClure
+              <br />
+
               <br />
               <i>
                 <b className="blue">
